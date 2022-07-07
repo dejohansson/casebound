a8abef1b6a713512627753ab09571f6df0f1b5a4
DEJohansson <33880373+DEJohansson@users.noreply.github.com>
2022-07-07 23:30:18 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { inject, onMounted, ref } from 'vue';
+import { inject, onMounted, onUnmounted, ref, watch, type Ref } from 'vue';
 import { LiteralApiClientKey } from './injectionKeys';
 import BookItem from './components/BookItem.vue';
 import ReadingStatus from './literal/models/readingStatus';
 import type LiteralApiClient from './literal/literalApiClient';
+import { getRandomInt } from './helpers';
 
 const id = import.meta.env.VITE_LITERAL_PROFILE_ID;
 const literalClient = inject(LiteralApiClientKey) as LiteralApiClient;
-const covers = ref();
+const covers: Ref<string[]> = ref([]);
+const nBooks = ref(
+  Math.ceil((window.outerWidth * window.outerHeight) / 200000)
+);
+
+onMounted(() => window.addEventListener('resize', setBookCount));
+
+onUnmounted(() => window.removeEventListener('resize', setBookCount));
+
+watch(nBooks, (nBooks, _) => {
+  console.log(nBooks);
+});
+
+function setBookCount() {
+  nBooks.value = Math.ceil((window.outerWidth * window.outerHeight) / 200000);
+}
 
 onMounted(async () => {
   covers.value = await literalClient.getAllCoversByReadingStateAndProfile(
@@ -15,10 +31,14 @@ onMounted(async () => {
     id
   );
 });
+
+function getNextCover() {
+  return covers.value[getRandomInt(0, covers.value.length - 1)];
+}
 </script>
 
 <template>
-  <BookItem v-for="cover in covers" :cover="cover" />
+  <BookItem v-for="index in 1" :key="index" :getNextCover="getNextCover" />
 </template>
 
 <style>
--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
-import { getRandomInt } from '@/helpers/math';
-import { computed } from 'vue';
+import { getRandomInt } from '@/helpers';
+import { computed, ref } from 'vue';
 
-defineProps<{
-  cover: string;
+const props = defineProps<{
+  getNextCover: () => string;
 }>();
+
+const cover = ref(props.getNextCover());
+
+setInterval(() => {
+  cover.value = props.getNextCover();
+}, 1000); //getRandomInt(1000, 25000));
 
 const styles = computed(() => {
   return {
-    left: `${getRandomInt(0, window.outerWidth)}px`,
-    top: `${getRandomInt(0, window.outerHeight)}px`,
-    '--z-index': `${getRandomInt(0, 700)}`,
+    // left: `${getRandomInt(0, window.outerWidth)}px`,
+    // top: `${getRandomInt(0, window.outerHeight)}px`,
+    // '--z-index': `${getRandomInt(0, 700)}`,
   };
 });
 </script>
 
 <template>
-  <img :src="cover" :style="styles" />
+  <transition name="hej">
+    <img v-if="true" :src="cover" :style="styles" />
+  </transition>
 </template>
 
 <style scoped>
@@ -24,7 +32,16 @@ img {
   margin-top: -100px;
   margin-left: -80px;
   max-width: 180px;
-  position: absolute;
-  z-index: var(--z-index);
+  /* position: absolute; */
+  /* z-index: var(--z-index); */
+}
+.hej-enter-from {
+  transform: translateX(20px);
+}
+.hej-enter-to {
+  transform: translateX(1000px);
+}
+.hej-enter-active {
+  transition: 10s;
 }
 </style>
