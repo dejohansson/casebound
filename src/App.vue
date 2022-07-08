6c0b7554069b9a7dfc9547400a725404618f5b3a
David Johansson <33880373+dejohansson@users.noreply.github.com>
2022-07-09 00:57:27 +0200

    Animate books from left to right (#12)
    * WIP
    
    * Animate books from left to right

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,51 @@
 <script setup lang="ts">
-import { inject, onMounted, ref } from 'vue';
+import { inject, onMounted, onUnmounted, ref, watch, type Ref } from 'vue';
 import { LiteralApiClientKey } from './injectionKeys';
 import BookItem from './components/BookItem.vue';
 import ReadingStatus from './literal/models/readingStatus';
 import type LiteralApiClient from './literal/literalApiClient';
+import { getRandomInt } from './helpers';
 
 const id = import.meta.env.VITE_LITERAL_PROFILE_ID;
 const literalClient = inject(LiteralApiClientKey) as LiteralApiClient;
-const covers = ref();
+const covers: Ref<string[]> = ref([]);
+const nBooks = ref(
+  Math.ceil((window.outerWidth * window.outerHeight) / 200000)
+);
 
 onMounted(async () => {
+  window.addEventListener('resize', setBookCount);
   covers.value = await literalClient.getAllCoversByReadingStateAndProfile(
     ReadingStatus.FINISHED,
     id
   );
 });
+
+onUnmounted(() => window.removeEventListener('resize', setBookCount));
+
+watch(nBooks, (nBooks, _) => {
+  console.log(nBooks);
+});
+
+function setBookCount() {
+  nBooks.value = Math.ceil((window.outerWidth * window.outerHeight) / 200000);
+}
+
+function getNextCover() {
+  return covers.value[getRandomInt(0, covers.value.length - 1)];
+}
+
+console.log(covers.value);
 </script>
 
 <template>
-  <BookItem v-for="cover in covers" :cover="cover" />
+  <BookItem
+    v-if="covers.length > 0"
+    v-for="index in nBooks"
+    :key="index"
+    :getNextCover="getNextCover"
+    :initialDelay="index"
+  />
 </template>
 
 <style>
--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -1,30 +1,64 @@
 <script setup lang="ts">
-import { getRandomInt } from '@/helpers/math';
-import { computed } from 'vue';
+import { getRandomInt } from '@/helpers';
+import { computed, ref, watch } from 'vue';
 
-defineProps<{
-  cover: string;
+const props = defineProps<{
+  getNextCover: () => string;
+  initialDelay: number;
 }>();
+
+const cover = ref(props.getNextCover());
+
+const show = ref(false);
+
+setTimeout(() => {
+  show.value = true;
+}, props.initialDelay * 1000);
+
+setTimeout(() => {
+  newBook();
+}, props.initialDelay * 1000 + 10000);
+
+function newBook() {
+  show.value = false;
+  cover.value = props.getNextCover();
+  setTimeout(() => {
+    show.value = true;
+    setTimeout(() => {
+      newBook();
+    }, 10000);
+  }, 1);
+}
 
 const styles = computed(() => {
   return {
-    left: `${getRandomInt(0, window.outerWidth)}px`,
-    top: `${getRandomInt(0, window.outerHeight)}px`,
+    top: `${getRandomInt(
+      -window.outerHeight * 0.2,
+      window.outerHeight * 0.8
+    )}px`,
     '--z-index': `${getRandomInt(0, 700)}`,
   };
 });
 </script>
 
 <template>
-  <img :src="cover" :style="styles" />
+  <transition name="slide">
+    <img v-if="show" :src="cover" :style="styles" />
+  </transition>
 </template>
 
 <style scoped>
 img {
-  margin-top: -100px;
-  margin-left: -80px;
-  max-width: 180px;
+  max-height: 30vh;
+  max-width: 60vw;
   position: absolute;
   z-index: var(--z-index);
+  transform: translateX(-100%);
+}
+.slide-enter-to {
+  transform: translateX(100vw);
+}
+.slide-enter-active {
+  transition: transform 10s linear;
 }
 </style>
